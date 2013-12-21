de9ad7b1dabbfb56a71534ef66016fcf45a8fce7
AmrARaouf <amr.abdelraouf93@gmail.com>
2013-12-21 20:15:55 +0200

    adds health packs and relative positions

--- MazePlusWa7sh/MazePlusWa7sh/MazePlusWa7sh.cpp
+++ MazePlusWa7sh/MazePlusWa7sh/MazePlusWa7sh.cpp
@@ -6,7 +6,17 @@ float szs[] = {11.2f, 2.6f, 6.0f, 6.0f, 1.8f, 1.8f, 9.8f, 0.4f, 0.4f, 0.4f, 0.4f
 float txs[] = {0.0f, 1.8f, 3.8f, 4.8f, 7.8f, 8.8f, 10.8f, 0.2f, 0.2f, 0.2f, 4.2f, 6.8f, 0.2f, 7.8f, 1.8f, 1.0f};
 float tzs[] = {0.0f, 8.2f, 1.0f, 1.0f, 6.0f, 2.0f, 1.0f, 0.0f, 1.8f, 3.8f, 3.8f, 3.8f, 5.8f, 7.8f, 7.8f, 10.8f};
 
+bool hp[] = {true, true, true};
+float hpx[] = {0.6f, 2.4f, 4.4f};
+float hpy[] = {3.4f, 8.4f, 4.4f};
+
+float px = 15.0f, pz = 0.7f;
+
+float health = 100;
+
 void wall(float sx, float sz, float tx, float tz) {
+	GLfloat mat_diffuse[] = { 0.6f, 0.6f, 0.6f, 1.0f };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
 	glPushMatrix();
 	glTranslatef(tx, 0, tz);
 	glScalef(sx, 1, sz);
@@ -15,17 +25,29 @@ void wall(float sx, float sz, float tx, float tz) {
 	glPopMatrix();
 }
 
+void healthPack(float tx, float tz) {
+	GLfloat mat_diffuse[] = { 1.0f, 0.0f, 0.0f, 0.0f };
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glPushMatrix();
+	glTranslatef(tx, 0, tz);
+	glScalef(0.15,0.1,0.15);
+	glTranslatef(0.5,0.5,0.5);
+	glutSolidCube(1);
+	glPopMatrix();
+}
+
 void SetupLights() {
 	GLfloat mat_ambient[] = { 0.7f, 0.7f, 0.7f, 1.0f };
-	GLfloat mat_diffuse[] = { 0.6f, 0.6f, 0.6f, 1.0f };
+	GLfloat mat_diffuse[] = { 1.0f, 0.6f, 0.6f, 1.0f };
 	GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0, 1.0f };
 	GLfloat mat_shininess[] = { 50 };
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
 	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
 	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+
 	GLfloat lightIntensity[] = { 0.7f, 0.7f, 1, 1.0f };
-	GLfloat light_position[] = { 24.0f, 0.5f, 1.7f, 1.0f };
+	GLfloat light_position[] = { px, 0.5f, pz, 1.0f };
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
 }
@@ -37,12 +59,17 @@ void display() {
 	gluPerspective(60, 1000 / 500, 0.001, 100);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(15, 0.5, 0.7, 10, 0.5, 0.7, 0.0, 1.0, 0.0);
+	gluLookAt(px, 0.5, pz, 10, 0.5, 0.7, 0.0, 1.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 	for(int i = 0; i <= 16; i++) {
 		wall(sxs[i], szs[i], txs[i], tzs[i]);
 	}
+	for(int i = 0; i <= 3; i++) {
+		if (hp[i]) {
+			healthPack(hpx[i], hpy[i]);
+		}
+	}
+
 	glFlush();
 }
 
