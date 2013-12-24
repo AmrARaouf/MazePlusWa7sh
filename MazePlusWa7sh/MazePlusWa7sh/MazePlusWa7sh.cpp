a98291dfb0083b3c1a1340c929feb5bd7aefc7a6
AmrARaouf <amr.abdelraouf93@gmail.com>
2013-12-24 20:00:13 +0200

    adds dllsand picture to right place

--- MazePlusWa7sh/MazePlusWa7sh/MazePlusWa7sh.cpp
+++ MazePlusWa7sh/MazePlusWa7sh/MazePlusWa7sh.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include <cstdlib>
+#include "glew.h"
+#include "glaux.h"
 #include "gl/glut.h"
 #include <iostream>
 
@@ -20,6 +22,46 @@ float health = 100, maxHealth = 100;
 
 float lastx=500, lasty=250;
 float angle = 0.0f;
+
+void CreateFromBMP(UINT *textureID, LPSTR strFileName) {
+	AUX_RGBImageRec *pBitmap = NULL;
+	FILE *pFile = NULL;           
+	// The File Handle we will use to read the bitmap
+	fopen_s(&pFile, strFileName,"r"); // Check To See If The File Exists
+	if(pFile) {
+		// If we have a valid file pointer we found the file
+		pBitmap = auxDIBImageLoadA(strFileName);  // Load the bitmap and store the data
+	} else {          
+	// If we can't find the file, quit! Prompt the error message
+		MessageBoxA(NULL, "Couldn't find a texture!", "Error!", MB_OK);
+		exit(0);
+	}
+	// Generate a texture with the associative texture ID stored in the array
+	glGenTextures(1, textureID);
+	// Bind the texture to the texture arrays index and init the texture
+	glBindTexture(GL_TEXTURE_2D, *textureID);
+	// Build Mipmaps (builds different versions of the picture for distances - looks better)
+	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, pBitmap->sizeX, pBitmap->sizeY, GL_RGB, GL_UNSIGNED_BYTE, 
+	pBitmap->data);
+	// Lastly, we need to tell OpenGL the quality of our texture map. 
+	GL_LINEAR_MIPMAP_LINEAR;
+	// is the smoothest. GL_LINEAR_MIPMAP_NEAREST is faster than GL_LINEAR_MIPMAP_LINEAR, 
+	// but looks blochy and pixilated. Good for slower computers though. 
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+	// The default GL_TEXTURE_WRAP_S and ""_WRAP_T property is GL_REPEAT.
+	// We need to turn this to GL_CLAMP_TO_EDGE, otherwise it creates ugly seems
+	// in our sky box. GL_CLAMP_TO_EDGE does not repeat when bound to an object.
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	// Now we need to free the bitmap data that we loaded since openGL stored it as a texture
+	if (pBitmap) { // If we loaded the bitmap
+		if (pBitmap->data) { //if there is texture data
+			free(pBitmap->data);// Free the texture data, we don't need it anymore
+		}
+		free(pBitmap); // Free the bitmap structure
+	}
+}
 
 void wall(float sx, float sz, float tx, float tz) {
 	GLfloat mat_diffuse[] = { 0.6f, 0.6f, 0.6f, 1.0f };
